95875415fa3869ddb93711362cba295cb96b51da
gstanescu <georgestanescu90@gmail.com>
2016-11-02 16:31:15 +0200

    grouped product-related elements to dedicated module

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 
-import { ProductService } from './products/product.service';
-
 @Component({
     selector: 'pm-app',
-    templateUrl: "app/app.component.html",
-    providers: [ ProductService ]
+    templateUrl: "app/app.component.html"
 })
 export class AppComponent {
     pageTitle: string = "Acme Product Management";
